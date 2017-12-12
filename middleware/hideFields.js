2223ce5c4d2a65dda74ed85cbaffb3d448bc0c46
rebelvg <johnhartigan@ya.ru>
2017-12-12 16:00:01 +0200

    Hide fields fix.

--- middleware/hideFields.js
+++ middleware/hideFields.js
@@ -2,12 +2,12 @@ const _ = require('lodash');
 
 const streamSchema = require('../schemas/stream');
 const subscriberSchema = require('../schemas/subscriber');
+const ipSchema = require('../schemas/ip');
 
 function hideFields(req, res, next) {
     _.set(streamSchema.options, ['toJSON', 'transform'], function (dec, ret, options) {
         if (!req.user) {
-            delete ret.ip;
-            delete ret.location;
+            ret.ip = '*';
         }
 
         return ret;
@@ -15,8 +15,16 @@ function hideFields(req, res, next) {
 
     _.set(subscriberSchema.options, ['toJSON', 'transform'], function (dec, ret, options) {
         if (!req.user) {
-            delete ret.ip;
-            delete ret.location;
+            ret.ip = '*';
+        }
+
+        return ret;
+    });
+
+    _.set(ipSchema.options, ['toJSON', 'transform'], function (dec, ret, options) {
+        if (!req.user) {
+            ret.ip = '*';
+            ret.api.query = '*';
         }
 
         return ret;
