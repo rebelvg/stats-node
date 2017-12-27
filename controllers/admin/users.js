4b8ef04b8e52b56bf082329b8e26fed94ff391f8
rebelvg <johnhartigan@ya.ru>
2017-12-27 18:14:11 +0200

    User update.

--- controllers/admin/users.js
+++ controllers/admin/users.js
@@ -3,7 +3,11 @@ const _ = require('lodash');
 const User = require('../../models/user');
 
 function find(req, res, next) {
-    User.find()
+    User.find(null, null, {
+        sort: {
+            createdAt: -1
+        }
+    })
         .then(users => {
             res.json({
                 users: users
@@ -12,4 +16,25 @@ function find(req, res, next) {
         .catch(next);
 }
 
+function update(req, res, next) {
+    User.findOne({
+        _id: req.params.id
+    })
+        .then(async user => {
+            if (!user) throw Error('User not found.');
+
+            _.forEach(req.body, (value, key) => {
+                user[key] = value;
+            });
+
+            await user.save();
+
+            res.json({
+                user: user
+            });
+        })
+        .catch(next);
+}
+
 exports.find = find;
+exports.update = update;
--- routes/admin/users.js
+++ routes/admin/users.js
@@ -5,5 +5,6 @@ let router = express.Router();
 const userController = require('../../controllers/admin/users');
 
 router.get('/', userController.find);
+router.put('/:id', userController.update);
 
 module.exports = router;
