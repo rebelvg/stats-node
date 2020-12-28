58696586e874962ae2c5f912ad8040de0a8232e6
Rebel <johnhartigan@ya.ru>
2020-12-28 18:16:39 +0200

    backfill scripts update

--- scripts/fix-user-ids.ts
+++ scripts/fix-user-ids.ts
@@ -5,11 +5,11 @@ import { IStreamModel } from '../src/models/stream';
 import { IIPModel } from '../src/models/ip';
 
 const REBEL_ID = '5a3a62c866fa07792cfcee67';
-const PYRO_ID = '5a3fb3f666fa07792cfceec2';
-const CREATURE_ID = '5b3a731ac284a806572fede5';
-const SLOW_ID = '5a3d93f066fa07792cfcee9c';
-const BOBER_ID = '5a7430e79b6aa006b6546f81';
-const OCTOCAT_ID = '5a6f7bc948f63728881611e0';
+// const PYRO_ID = '5a3fb3f666fa07792cfceec2';
+// const CREATURE_ID = '5b3a731ac284a806572fede5';
+// const SLOW_ID = '5a3d93f066fa07792cfcee9c';
+// const BOBER_ID = '5a7430e79b6aa006b6546f81';
+// const OCTOCAT_ID = '5a6f7bc948f63728881611e0';
 
 (async () => {
   const mongoClient = await connectMongoDriver();
@@ -27,7 +27,7 @@ const OCTOCAT_ID = '5a6f7bc948f63728881611e0';
     const ipRecord = await ipsCollection.findOne({ ip });
 
     const location = ipRecord.api.city || ipRecord.api.message;
-    const isp = ipRecord.api.isp;
+    // const isp = ipRecord.api.isp;
 
     switch (location) {
       case 'Kharkiv': {
@@ -35,6 +35,8 @@ const OCTOCAT_ID = '5a6f7bc948f63728881611e0';
           { userId: null, ip },
           { $set: { userId: new ObjectId(REBEL_ID) } },
         );
+
+        console.log(ip);
 
         break;
       }
@@ -44,6 +46,8 @@ const OCTOCAT_ID = '5a6f7bc948f63728881611e0';
           { $set: { userId: new ObjectId(REBEL_ID) } },
         );
 
+        console.log(ip);
+
         break;
       }
       case 'reserved range': {
@@ -52,71 +56,73 @@ const OCTOCAT_ID = '5a6f7bc948f63728881611e0';
           { $set: { userId: new ObjectId(REBEL_ID) } },
         );
 
-        break;
-      }
-      case 'Odesa': {
-        await streamsCollection.updateMany(
-          { userId: null, ip },
-          { $set: { userId: new ObjectId(PYRO_ID) } },
-        );
+        console.log(ip);
 
         break;
       }
-      case 'Gomel': {
-        await streamsCollection.updateMany(
-          { userId: null, ip },
-          { $set: { userId: new ObjectId(CREATURE_ID) } },
-        );
+      // case 'Odesa': {
+      //   await streamsCollection.updateMany(
+      //     { userId: null, ip },
+      //     { $set: { userId: new ObjectId(PYRO_ID) } },
+      //   );
 
-        break;
-      }
-      case 'Moscow': {
-        if (isp.toLowerCase().includes('mediaseti')) {
-          await streamsCollection.updateMany(
-            { userId: null, ip },
-            { $set: { userId: new ObjectId(SLOW_ID) } },
-          );
-        }
+      //   break;
+      // }
+      // case 'Gomel': {
+      //   await streamsCollection.updateMany(
+      //     { userId: null, ip },
+      //     { $set: { userId: new ObjectId(CREATURE_ID) } },
+      //   );
 
-        if (isp.toLowerCase().includes('telegraph')) {
-          await streamsCollection.updateMany(
-            { userId: null, ip },
-            { $set: { userId: new ObjectId(BOBER_ID) } },
-          );
-        }
+      //   break;
+      // }
+      // case 'Moscow': {
+      //   if (isp.toLowerCase().includes('mediaseti')) {
+      //     await streamsCollection.updateMany(
+      //       { userId: null, ip },
+      //       { $set: { userId: new ObjectId(SLOW_ID) } },
+      //     );
+      //   }
 
-        if (isp.toLowerCase().includes('2com')) {
-          await streamsCollection.updateMany(
-            { userId: null, ip },
-            { $set: { userId: new ObjectId(OCTOCAT_ID) } },
-          );
-        }
+      //   if (isp.toLowerCase().includes('telegraph')) {
+      //     await streamsCollection.updateMany(
+      //       { userId: null, ip },
+      //       { $set: { userId: new ObjectId(BOBER_ID) } },
+      //     );
+      //   }
 
-        if (ip.includes('109.229')) {
-          await streamsCollection.updateMany(
-            { userId: null, ip },
-            { $set: { userId: new ObjectId(SLOW_ID) } },
-          );
-        }
+      //   if (isp.toLowerCase().includes('2com')) {
+      //     await streamsCollection.updateMany(
+      //       { userId: null, ip },
+      //       { $set: { userId: new ObjectId(OCTOCAT_ID) } },
+      //     );
+      //   }
 
-        break;
-      }
-      case 'Shcherbinka': {
-        await streamsCollection.updateMany(
-          { userId: null, ip },
-          { $set: { userId: new ObjectId(SLOW_ID) } },
-        );
+      //   if (ip.includes('109.229')) {
+      //     await streamsCollection.updateMany(
+      //       { userId: null, ip },
+      //       { $set: { userId: new ObjectId(SLOW_ID) } },
+      //     );
+      //   }
 
-        break;
-      }
-      case 'Balashikha': {
-        await streamsCollection.updateMany(
-          { userId: null, ip },
-          { $set: { userId: new ObjectId(BOBER_ID) } },
-        );
+      //   break;
+      // }
+      // case 'Shcherbinka': {
+      //   await streamsCollection.updateMany(
+      //     { userId: null, ip },
+      //     { $set: { userId: new ObjectId(SLOW_ID) } },
+      //   );
 
-        break;
-      }
+      //   break;
+      // }
+      // case 'Balashikha': {
+      //   await streamsCollection.updateMany(
+      //     { userId: null, ip },
+      //     { $set: { userId: new ObjectId(BOBER_ID) } },
+      //   );
+
+      //   break;
+      // }
       default: {
         break;
       }
