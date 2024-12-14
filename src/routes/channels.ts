5b79d6818cea947dc8580b08ed12876edbff216c
Volodymyr D. <9673665+rebelvg@users.noreply.github.com>
2024-12-14 01:30:03 +0100

    disable public endpoints

--- src/routes/channels.ts
+++ src/routes/channels.ts
@@ -1,9 +1,10 @@
 import * as Router from 'koa-router';
 
 import { appChannelStats, channels, list } from '../controllers/channels';
+import { isAdmin } from '../middleware/is-admin';
 
 export const router = new Router();
 
 router.get('/:server/:app/:channel', appChannelStats);
-router.get('/list', list);
-router.get('/', channels);
+router.get('/list', isAdmin, list);
+router.get('/', isAdmin, channels);
